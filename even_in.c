90ec646cd7e4824817a2ad3a0ba8915791f3621c
jpoongodi <punithajeeva2013@gmail.com>
2016-06-16 14:30:51 +0530

    Create even_in.c

---
+++ even_in.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int l,r,i;
+    scanf("%d %d",&l,&r);
+   
+   
+    for(i=l+1;i<r;i++)
+    {
+        if(i%2==0)
+         printf("%d ",i);
+    }
+    
+}
